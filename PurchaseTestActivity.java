ef95b1a65d3e09188a3c2394645eae5c950bb993
Aby Mathew <vabym8@gmail.com>
2016-12-10 16:09:14 +0300

    Update and rename PurchaseTestActivity to PurchaseTestActivity.java

--- PurchaseTestActivity.java
+++ PurchaseTestActivity.java
@@ -26,7 +26,7 @@ public class PurchaseTestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchase_show_case);
+        setContentView(R.layout.activity_purchase_test);
 
         one = (Button) findViewById(R.id.btnOne);
         two = (Button) findViewById(R.id.btnTwo);
